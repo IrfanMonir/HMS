3636aee3bdc1ad51ff9c63a099cfe9bae2dc313c
IrfanMonir <irfanuddin211@gmail.com>
2020-04-04 22:51:25 +0600

    Template integrated for frontend users

---
+++ HMSNew/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMSNew.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Irfan International Resort";
+        public const string HotelShortName = "Irfan";
+        public const string HotelAbbreviateName = "IIR";
+    }
+}
